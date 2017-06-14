13d0f06b1c2499778ae9384f1ae4fab8fd066ba9
clarkmcadoo <clark.mcadoo@gmail.com>
2017-06-14 16:58:22 -0500

    Functions work, pulling 12 customers now

--- main.js
+++ main.js
@@ -1,21 +1,53 @@
 // 1. Fetch your users data
 // 2. Loop over the data
 // 3. Display it in the `.customers` element
-var customer = document.querySelector(".customer");
+var customer = document.querySelector(".customers");
+var customerInfo;
+(function() {
+  "use strict";
+  fetch("https://randomuser.me/api/?results=12")
+    .then(function(customerData) {
+      return customerData.json();
+    })
+    .then(function(data) {
+      console.log(data.results);
+      var customerInfo = data.results;
+      for (let i = 0; i < 12; i++) {
+        buildCustomer(customerInfo[i]); //Using function below
+      }
+    });
 
-(function () {
-  fetch("https://randomuser.me/api/").then(function(customerData){
-  console.log(customerData);  
-  return customerData.json();  
-}).then(function(data){
-  console.log(data.results);
-  var customerInfo = data.results;
-  customer.innerHTML = customerInfo[0].name.first + " " + customerInfo[0].name.last;
+  function buildCustomer(array) {
+    var custProfile = document.createElement("div");
+    customer.appendChild(custProfile);
 
+    var custPhoto = document.createElement("img");
+    custProfile.appendChild(custPhoto);
+    custPhoto.src = array.picture.large;
 
-  });
-  'use strict';
+    var custName = document.createElement("h3");
+    custProfile.appendChild(custName);
+    custName.innerHTML = array.name.first + " " + array.name.last;
 
+    var custEmail = document.createElement("p");
+    custProfile.appendChild(custEmail);
+    custEmail.innerHTML = array.email;
 
+    var custEmail = document.createElement("p");
+    custProfile.appendChild(custEmail);
+    custEmail.innerHTML = array.email;
 
+    var custStreet = document.createElement("p");
+    custProfile.appendChild(custStreet);
+    custEmail.innerHTML = array.location.street;
+
+    var custCity = document.createElement("p");
+    custProfile.appendChild(custCity);
+    custCity.innerHTML =
+      array.location.city +
+      ", " +
+      array.location.state +
+      ", " +
+      array.location.postcode;
+  }
 })();
