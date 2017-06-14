775680bf24cf968d2bd6f9295a1b2b80613d5eb0
clarkmcadoo <clark.mcadoo@gmail.com>
2017-06-14 15:42:57 -0500

    Got name to write to HTML

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // 1. Fetch your users data
 // 2. Loop over the data
 // 3. Display it in the `.customers` element
-(function () {
+var customer = document.querySelector(".customer");
 
+(function () {
+  fetch("https://randomuser.me/api/").then(function(customerData){
+  console.log(customerData);  
+  return customerData.json();  
+}).then(function(data){
+  console.log(data.results);
+  var customerInfo = data.results;
+  customer.innerHTML = customerInfo[0].name.first + " " + customerInfo[0].name.last;
+
+
+  });
   'use strict';
 
-  // Your Code Goes Here
+
 
 })();
